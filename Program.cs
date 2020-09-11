d562c4452f445c97c25e2ff51abc1a221f786274
jbpratt78 <jbpratt78@gmail.com>
2020-09-11 11:13:14 -0500

    inital commit, playing with toplevel programs

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+
+do
+{
+    using (var socket = new ClientWebSocket())
+    {
+        try
+        {
+            await socket.ConnectAsync(new Uri("wss://chat.strims.gg/ws"), CancellationToken.None);
+            var buff = new ArraySegment<byte>(new byte[2048]);
+            do
+            {
+                WebSocketReceiveResult result;
+                using (var ms = new MemoryStream())
+                {
+                    do
+                    {
+                        result = await socket.ReceiveAsync(buff, CancellationToken.None);
+                        ms.Write(buff.Array, buff.Offset, result.Count);
+                    } while (!result.EndOfMessage);
+
+                    if (result.MessageType == WebSocketMessageType.Close) break;
+
+                    ms.Seek(0, SeekOrigin.Begin);
+                    using (var reader = new StreamReader(ms, Encoding.UTF8))
+                    {
+                        string data = await reader.ReadToEndAsync();
+                        var received = data.Split();
+                        var msg = string.Join(" ", received.Skip(1));
+                        switch (received[0])
+                        {
+                            case "MSG":
+                                Console.WriteLine(msg);
+                                break;
+                            case "NAMES":
+                            case "JOIN":
+                            case "QUIT":
+                            case "VIEWERSTATE":
+                                break;
+                            default:
+                                Console.WriteLine("wat");
+                                break;
+                        }
+                    }
+                }
+            } while (true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"ERROR - {ex.Message}");
+        }
+    }
+} while (true);
---
+++ README.md
@@ -0,0 +1,3 @@
+### Setup
+
+Download and install the [5.0 preview](https://dotnet.microsoft.com/download/dotnet/5.0)
